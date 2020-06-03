0a46611f8c22abe21106ee4abf39a024af2deded
Jens Krause <jens@rasta.online>
2020-06-03 16:34:27 +0700

    use ApplicatopnProvider to get ApplicationContext

--- app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
@@ -1,14 +1,19 @@
 package com.example.android.architecture.blueprints.todoapp.tasks
 
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.junit.runner.RunWith
 
+@RunWith(AndroidJUnit4::class)
 class TasksViewModelTest {
 
     @Test
     fun addNewTask_setsNewTaskEvent() {
         // Given a fresh TasksViewModel
+        val taskViewModel = TasksViewModel(ApplicationProvider.getApplicationContext())
 
         // When adding a new task
 

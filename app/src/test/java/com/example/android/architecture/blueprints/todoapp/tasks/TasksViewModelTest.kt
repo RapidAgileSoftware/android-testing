f523a683b8fe1b369f9447f2794a9360c3a8640a
Jens Krause <jens@rasta.online>
2020-06-04 13:31:46 +0700

    LiveData testing: a) add InstantTaskExecutor rule b) use getOrAwaitValue() extension function to retrieve observable Live data

--- app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
@@ -1,14 +1,23 @@
 package com.example.android.architecture.blueprints.todoapp.tasks
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.android.architecture.blueprints.todoapp.getOrAwaitValue
+import org.hamcrest.CoreMatchers.not
+import org.hamcrest.CoreMatchers.nullValue
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.junit.Rule
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class TasksViewModelTest {
+
+    // for testing live data
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
 
     @Test
     fun addNewTask_setsNewTaskEvent() {
@@ -16,7 +25,12 @@ class TasksViewModelTest {
         val taskViewModel = TasksViewModel(ApplicationProvider.getApplicationContext())
 
         // When adding a new task
+        taskViewModel.addNewTask()
 
         // Then a new task event is triggered
+        val value = taskViewModel.newTaskEvent.getOrAwaitValue()
+        // value is an Event, getContentIfNotHandled gets its content
+        // we assert that it is not null
+        assertThat(value.getContentIfNotHandled(),(not(nullValue())))
     }
 }

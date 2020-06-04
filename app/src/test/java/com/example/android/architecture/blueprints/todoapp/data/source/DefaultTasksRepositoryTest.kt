f2360a56d37c8cc63dadac069b7dbb08ac16d1a8
Jens Krause <jens@rasta.online>
2020-06-04 16:15:08 +0700

    use FakeDatasource in test and add library for running suspended functions /coroutines in test

--- app/src/test/java/com/example/android/architecture/blueprints/todoapp/data/source/DefaultTasksRepositoryTest.kt
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/data/source/DefaultTasksRepositoryTest.kt
@@ -1,5 +1,54 @@
 package com.example.android.architecture.blueprints.todoapp.data.source
 
+import com.example.android.architecture.blueprints.todoapp.data.Result.Error
+import com.example.android.architecture.blueprints.todoapp.data.Result.Success
+import com.example.android.architecture.blueprints.todoapp.data.Task
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
+import org.hamcrest.core.IsEqual
 import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Test
 
-class DefaultTasksRepositoryTest
+class DefaultTasksRepositoryTest {
+    private val task1 = Task("Task1", "description 1")
+    private val task2 = Task("Task2", "description 2")
+    private val task3 = Task("Task3", "description 3")
+
+    private val remoteTasks = listOf(task1, task2).sortedBy { it.id }
+    private val localTasks = listOf(task3).sortedBy { it.id }
+    private val newTasks = listOf(task3).sortedBy { it.id }
+
+    private lateinit var taskRemoteDataSource: FakeDataSource
+    private lateinit var taskLocalDataSource: FakeDataSource
+    // class to test
+    private lateinit var tasksRepository: DefaultTasksRepository
+
+    @Before
+    fun createRepository(){
+        taskRemoteDataSource = FakeDataSource(remoteTasks.toMutableList())
+        taskLocalDataSource = FakeDataSource(localTasks.toMutableList())
+        // here we init our class to test
+        tasksRepository= DefaultTasksRepository(
+                taskRemoteDataSource,
+                taskLocalDataSource,
+                Dispatchers.Unconfined
+        )
+    }
+    // we use CoroutinesApi to run suspended functions with runBlockingTest
+    @ExperimentalCoroutinesApi
+    @Test
+    fun getTasks_requestsAllTasksFromRemoteDataSource()= runBlockingTest{
+        // When tasks are requested from the tasks repository
+        val tasks = tasksRepository.getTasks(true) as Success
+
+        // Then tasks are loaded from the remote data source
+        assertThat(tasks.data, IsEqual(remoteTasks))
+
+    }
+
+
+
+
+}

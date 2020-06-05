fde6237feb0524f22e0b888ace31e8be40f6114f
Jens Krause <jens@rasta.online>
2020-06-05 15:29:53 +0700

    lauch Task Detail Fragment in Container

--- app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/taskdetail/TaskDetailFragmentTest.kt
+++ app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/taskdetail/TaskDetailFragmentTest.kt
@@ -1,5 +1,29 @@
 package com.example.android.architecture.blueprints.todoapp.taskdetail
 
+import androidx.fragment.app.testing.launchFragment
+import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.test.filters.MediumTest
+import com.example.android.architecture.blueprints.todoapp.R
+import com.example.android.architecture.blueprints.todoapp.data.Task
 import org.junit.Assert.*
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
 
-class TaskDetailFragmentTest
+@RunWith(JUnit4::class)
+//MediumTest needs a medium amount of time to run
+@MediumTest
+class TaskDetailFragmentTest(){
+
+    @Test
+    fun activeTaskDetails_DisplayedInUI(){
+        // Given is an active (= not completed) task to the DB
+        val activeTask = Task("Active Task", "let's do something awesome", false)
+
+        // When Details fragment launched to display task
+        val bundle = TaskDetailFragmentArgs(activeTask.id).toBundle()
+        launchFragmentInContainer<TaskDetailFragment>(bundle, R.style.AppTheme)
+        Thread.sleep(2000)
+    }
+
+}

934429d7bc12b982f9efffc3663941ddf93f76de
Jens Krause <jens@rasta.online>
2020-06-05 15:53:17 +0700

    create ServiceLocator which provides a TaskRepository and implicit a local data source(=DB)

---
+++ app/src/main/java/com/example/android/architecture/blueprints/todoapp/ServiceLocator.kt
@@ -0,0 +1,47 @@
+package com.example.android.architecture.blueprints.todoapp
+
+import android.content.Context
+import androidx.room.Room
+import com.example.android.architecture.blueprints.todoapp.data.source.DefaultTasksRepository
+import com.example.android.architecture.blueprints.todoapp.data.source.ITasksRepository
+import com.example.android.architecture.blueprints.todoapp.data.source.TasksDataSource
+import com.example.android.architecture.blueprints.todoapp.data.source.local.TasksLocalDataSource
+import com.example.android.architecture.blueprints.todoapp.data.source.local.ToDoDatabase
+import com.example.android.architecture.blueprints.todoapp.data.source.remote.TasksRemoteDataSource
+
+// we are using object to make it a singleton
+object ServiceLocator {
+    private var database: ToDoDatabase? = null
+
+    // volatile because it could be used by multiple threads
+    @Volatile
+    var tasksRepository:ITasksRepository? = null
+
+    // we need to context to create a DB
+    fun provideTaskRepository(context:Context):ITasksRepository{
+        // synchronized because we have potentially multiple threads working with it
+        synchronized(this){
+            return tasksRepository ?: createTasksRepository(context)
+        }
+    }
+
+    private fun createTasksRepository(context: Context): ITasksRepository {
+        val newRepo = DefaultTasksRepository(TasksRemoteDataSource, createTaskLocalDataSource(context))
+        tasksRepository = newRepo
+        return newRepo
+    }
+
+    private fun createTaskLocalDataSource(context: Context): TasksDataSource {
+        val database = database ?: createDataBase(context)
+        return TasksLocalDataSource(database.taskDao())
+    }
+
+    private fun createDataBase(context: Context): ToDoDatabase {
+        val result = Room.databaseBuilder(
+                context.applicationContext,
+                ToDoDatabase::class.java, "Tasks.db"
+        ).build()
+        database = result
+        return result
+    }
+}

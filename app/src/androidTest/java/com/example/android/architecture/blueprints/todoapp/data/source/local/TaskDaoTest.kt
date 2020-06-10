9d32e0c78c17b7887ec1954b65e630f496834289
Jens Krause <jens@rasta.online>
2020-06-10 14:21:57 +0700

    write DAO test for inserting and retrieving a task

--- app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/data/source/local/TaskDaoTest.kt
+++ app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/data/source/local/TaskDaoTest.kt
@@ -5,10 +5,16 @@ import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.SmallTest
+import com.example.android.architecture.blueprints.todoapp.data.Task
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
+import org.hamcrest.CoreMatchers.notNullValue
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.core.Is.`is`
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
 
 @ExperimentalCoroutinesApi
@@ -31,5 +37,23 @@ class TaskDaoTest (){
 
     @After
     fun closeDB() = database.close()
+
+    @Test
+    fun insertTaskAndGetItById()= runBlockingTest{
+        // Given - insert a Task
+        val task = Task("Task 1", "Description 1")
+        database.taskDao().insertTask(task)
+
+        // When - getting the task by Id from DB
+        val loadedTask = database.taskDao().getTaskById(task.id)
+
+        // Then - it has the correct values
+        assertThat<Task>(loadedTask as Task, notNullValue())
+        assertThat(loadedTask?.id, `is`(task.id))
+        assertThat(loadedTask?.title, `is`("Task 1"))
+        assertThat(loadedTask?.description, `is`("Description 1"))
+        assertThat(loadedTask?.isCompleted, `is`(task.isCompleted))
+
+    }
 
 }
